16b3322b7766d4b8458f9c2e643798789aebc4ff
Viodine <viodine@yahoo.com>
2019-06-18 20:36:11 +0200

    Modify SignUp form to add HighSchool select

--- app/models/matura_result.rb
+++ app/models/matura_result.rb
@@ -1,7 +1,6 @@
 class MaturaResult < ApplicationRecord
   enum level: %i[basic advanced]
   validates :level, :result, presence: true
-  validates :level, :result, numericality: { only_integers: true }
   validates_inclusion_of :result, in: 0..100, on: :create
   belongs_to :matura_subject
   belongs_to :user
