c34ccc0b77ac50c7fbac5dc4ba462cf2a4ba3d54
Viodine <viodine@yahoo.com>
2019-06-24 21:01:18 +0200

    Add temporary field_types to FieldOfStudy. Should refactor it to new table

--- app/models/field_of_study.rb
+++ app/models/field_of_study.rb
@@ -1,5 +1,8 @@
 class FieldOfStudy < ApplicationRecord
-  enum field_type: %i[humanities social_sciences natural_sciences formal_sciences professions others]
+  enum field_type: %i[humanities administrative artistic medical social_sciences
+                      natural_sciences formal_sciences technical sports 
+                      linguistic economical pedagogical law psychological 
+                      military agricultural others] # Should refactor to new model :/
   validates :name, :field_type, presence: true
   validates :field_type, numericality: { only_integers: true }
   validates :name, uniqueness: { case_sensitive: false }
