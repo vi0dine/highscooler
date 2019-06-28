adbb816aa049124e51cc60de587c316ac24cec65
Viodine <viodine@yahoo.com>
2019-06-28 10:57:01 +0200

    Small fixes with user model, specs and dashboard controller to improve counting algorithm

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,31 +1,26 @@
 class Users::DashboardController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @fields = []
-    @formulas = []
-    @user.field_of_studies.each do |fos|
-      fos.field_details.each do |fd|
-        @fields << fd
-        @formulas << fd.recrutation_formula
-      end
-    @formulas << count_points(@user, @formulas)
-    end
+    @fields = @user.field_details
+    @formulas = @user.field_details.collect(&:recrutation_formula)
   end
 
   private
 
   # RegEx 1: \(([^()]+)\) -> Match elements in parentheses
   # RegEx 2: [^(].+?(#{subject.name}).+?(?=\))
-
+  # RegEx 3: \(.*?(#{subject.name}).*?\)
+  # RegEx 4: \((?:(?!#{subject_to_not_include}).)*?\)
+  
   def count_points(user, formulas)
     formulas.each do |formula|
       user.matura_subjects.each do |subject|
-        if (formula.include? "#{subject.name}_Pp") && 
+        if (formula.include? "#{subject.name}_Pp") &&
            !user.matura_results.select {|res| res.matura_subject_id == subject.id && res.level == 'basic'}.nil?
-          formula.gsub!(/[^(].*?(#{subject.name}).*?(?=\))/, "100".to_s)
+          formula.gsub!("#{subject.name}_Pp", '100'.to_s)
         elsif (formula.include? "#{subject.name}_Pr") && 
               !user.matura_results.select {|res| res.matura_subject_id == subject.id && res.level == 'advanced'}.nil?
-          formula.gsub!(/[^(].*?(#{subject.name}).*?(?=\))/, "100".to_s)
+          formula.gsub!("#{subject.name}_Pr", '100'.to_s)
         end
       end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :study_interests
   has_many :matura_subjects, -> { distinct }, through: :matura_results
   has_many :field_of_studies, through: :study_interests
+  has_many :field_details, through: :field_of_studies
   belongs_to :high_school
   accepts_nested_attributes_for :matura_results
   accepts_nested_attributes_for :study_interests
