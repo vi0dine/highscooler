aada3c8d15d9aff21592218f67337b1794ae5d7d
Viodine <viodine@yahoo.com>
2019-09-14 12:50:43 +0200

    Add tests for :show in AcademiesController

--- spec/controllers/academies_controller_spec.rb
+++ spec/controllers/academies_controller_spec.rb
@@ -14,9 +14,28 @@ RSpec.describe AcademiesController, type: :controller do
     it { should render_template(:index) }
 
     it 'assign @academies' do
-      academy = FactoryBot.create(:academy)
+      academies = FactoryBot.create_list(:academy, 8)
       get :index
-      expect(assigns(:academies)).to eq([academy])
+      expect(assigns(:academies)).to eq(academies[0..4])
+    end
+  end
+
+  describe 'GET :show' do
+    before(:each) do
+      @academy = FactoryBot.create(:academy)
+      get :show, params: { id: @academy.id }
+    end
+
+    it { should respond_with(200) }
+    it { should render_template(:show) }
+
+    it 'assign @academy' do
+      expect(assigns(:academy)).to eq(@academy)
+    end
+
+    it 'render academy info' do
+      expect(response.body).to include(@academy.name.to_s)
+      expect(response.body).to include(@academy.city.to_s)
     end
   end
 end
