6b1b06b255344c6bf5574a0d4ae4417d653cd941
Viodine <viodine@yahoo.com>
2019-09-15 16:38:54 +0200

    Small changes in models validations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
       can :read, User, id: user.id
       can :read, FieldOfStudy
       can :read, Academy
+      can :create, Interested, user_id: user.id
       if user.account_type == 'student'
         can :create, FieldOpinion, field_of_study_id: user.field_of_study_id
         can :create, AcademyOpinion, academy_id: user.academy_id
--- app/models/interested.rb
+++ app/models/interested.rb
@@ -4,4 +4,5 @@ class Interested < ApplicationRecord
   belongs_to :field_detail
   belongs_to :user
   validates_uniqueness_of :user_id, scope: :field_detail_id
+  validates :user_id, :field_detail_id, presence: true
 end
