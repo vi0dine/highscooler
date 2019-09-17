edc76e92030c68ad428c32411fb0e7f142655611
Viodine <viodine@yahoo.com>
2019-09-17 15:42:39 +0200

    Improve UX on registration form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize, f: builder)
+      render(association.to_s.singularize, f: builder, level: args[:level])
     end
     link_to(name, '#', class: 'add_exam ' + args[:class], data: {id: id, fields: fields.gsub('\n', '')})
   end
