485af7bf04bf0aee9a5ce7761cf13358455d5ee3
Viodine <viodine@yahoo.com>
2019-10-06 20:21:53 +0200

    Add basic test for RecrutationCalculation service

--- app/services/recrutation_points_calculator.rb
+++ app/services/recrutation_points_calculator.rb
@@ -3,7 +3,7 @@
 class RecrutationPointsCalculator
   def initialize(user)
     @user = user
-    # puts "Calculating points for #{user.username}"
+    puts "Calculating points for #{user.username}"
   end
 
   def self.call(user)
@@ -17,6 +17,76 @@ class RecrutationPointsCalculator
   end
 
   private
+
+  def calculate_recrutation_points(user)
+    puts 'Start calculating...'
+    counted = count_points(user)
+    counted.each do |points|
+      points.gsub!(/\+-1\+/, '+0+')
+    end
+    assign_recrutation_results(user, counted)
+    user.results
+  end
+
+  def count_points(user)
+    # For each formula
+    user.formulas.each do |formula|
+      @basic_res = user.matura_results.select(&:basic?)
+      @advanced_res = user.matura_results.select(&:advanced?)
+      @max_basic_subject_id = nil
+      @max_advanced_subject_id = nil
+      @max_exponent_result = -1
+      puts "Counting points for formula #{formula}"
+      catch :formula_error do
+        exps = formula.split('+')
+        formula.clear
+        # and each element
+        exps.each { |exp| calculate_exponent(exp, formula) }
+      end
+    end
+  end
+
+  def calculate_exponent(exp, formula)
+    puts "Calculating exponent #{exp}"
+    @max_exponent_result = -1
+    exp.gsub!(/^[\[]|[\]]$/, '')
+    # extract subjects
+    subjects = exp.split('|')
+    puts 'Extracted subjects:'
+    # subjects.each do |subject|
+    #   puts subject
+    # end
+    # and for each subjects
+    subjects.each { |subject| replace_subject_name(subject) }
+    # replace first element with @max_exponent_result value
+    exp.replace(@max_exponent_result.to_s)
+    # reject @max_exponent_result elements from user results to avoid duplicates
+    @basic_res.reject! { |res| res.id == @max_basic_subject_id }
+    @advanced_res.reject! { |res| res.id == @max_advanced_subject_id }
+    # replace formula with calculated elements
+    formula << exp << '+'
+    puts "Converted formula: #{formula}"
+    puts "Basic res: #{@basic_res&.count}"
+    puts "Advanced res: #{@advanced_res&.count}"
+  end
+
+  def replace_subject_name(subject)
+    subject.gsub!(/^[\(]|[\)]$/, '')
+    subject_name = /.*_../.match(subject)[0].to_s
+    puts "Subject: #{subject_name} | Basic: #{find_subject_with_level('basic', subject_name)} | Advanced: #{find_subject_with_level('advanced', subject_name)}"
+    # replace its name with users result
+    if find_subject_with_level('basic', subject_name)
+      puts 'Found basic result'
+      update_max_basic_result(subject, subject_name)
+    elsif find_subject_with_level('advanced', subject_name)
+      puts 'Found advanced result'
+      update_max_advanced_result(subject, subject_name)
+    else
+      puts 'Assigning 0'
+      # if no user result assign '0'
+      subject.gsub!(subject_name, '0')
+    end
+  end
 
   def find_subject_with_level(level, subject_name)
     if level == 'basic'
@@ -54,75 +124,6 @@ class RecrutationPointsCalculator
       # p 'Syntax error in formula'
       throw :formula_error
     end
-  end
-
-  def replace_subject_name(subject)
-    subject.gsub!(/^[\(]|[\)]$/, '')
-    subject_name = /.*_../.match(subject)[0].to_s
-    # puts "Subject: #{subject_name} | Basic: #{find_subject_with_level('basic', subject_name)} | Advanced: #{find_subject_with_level('advanced', subject_name)}"
-    # replace its name with users result
-    if find_subject_with_level('basic', subject_name)
-      # puts 'Found basic result'
-      update_max_basic_result(subject, subject_name)
-    elsif find_subject_with_level('advanced', subject_name)
-      # puts 'Found advanced result'
-      update_max_advanced_result(subject, subject_name)
-    else
-      # puts 'Assigning 0'
-      # if no user result assign '0'
-      subject.gsub!(subject_name, '0')
-    end
-  end
-
-  def calculate_exponent(exp, formula)
-    # puts "Calculating exponent #{exp}"
-    @max_exponent_result = -1
-    exp.gsub!(/^[\[]|[\]]$/, '')
-    # extract subjects
-    subjects = exp.split('|')
-    # puts 'Extracted subjects:'
-    # subjects.each do |subject|
-    #   puts subject
-    # end
-    # and for each subjects
-    subjects.each { |subject| replace_subject_name(subject) }
-    # replace first element with @max_exponent_result value
-    exp.replace(@max_exponent_result.to_s)
-    # reject @max_exponent_result elements from user results to avoid duplicates
-    @basic_res.reject! { |res| res.id == @max_basic_subject_id }
-    @advanced_res.reject! { |res| res.id == @max_advanced_subject_id }
-    # replace formula with calculated elements
-    formula << exp << '+'
-    # puts "Converted formula: #{formula}"
-    # puts "Basic res: #{@basic_res&.count}"
-    # puts "Advanced res: #{@advanced_res&.count}"
-  end
-
-  def count_points(user)
-    # For each formula
-    user.formulas.each do |formula|
-      @basic_res = user.matura_results.select(&:basic?)
-      @advanced_res = user.matura_results.select(&:advanced?)
-      @max_basic_subject_id = nil
-      @max_advanced_subject_id = nil
-      @max_exponent_result = -1
-      # puts "Counting points for formula #{formula}"
-      catch :formula_error do
-        exps = formula.split('+')
-        formula.clear
-        # and each element
-        exps.each { |exp| calculate_exponent(exp, formula) }
-      end
-    end
-  end
-
-  def calculate_recrutation_points(user)
-    # puts 'Start calculating...'
-    counted = count_points(user)
-    counted.each do |points|
-      points.gsub!(/\+-1\+/, '+0+')
-    end
-    assign_recrutation_results(user, counted)
   end
 
   def assign_recrutation_results(user, prepared_formulas)
--- spec/services/recrutation_points_calculator_spec.rb
+++ spec/services/recrutation_points_calculator_spec.rb
@@ -3,23 +3,94 @@
 require 'rails_helper'
 require './app/services/recrutation_points_calculator'
 
-describe RecrutationPointsCalculator do\
-  let(:user) { create(:user) }
+describe RecrutationPointsCalculator do
   before {
-    physics = create(:field_of_study, name: 'Fizyka')
-    issp = create(:field_of_study, name: 'ISSP')
-    biotechnology = create(:field_of_study, name: 'Biotechnologia')
-    med = create(:field_of_study, name: 'Lekarski')
+    @physics = create(:field_of_study, name: 'Fizyka')
+    @issp = create(:field_of_study, name: 'ISSP')
+    @biotechnology = create(:field_of_study, name: 'Biotechnologia')
+    @med = create(:field_of_study, name: 'Lekarski')
 
-    create(:field_detail, field_of_study: physics, recrutation_formula: "[(Fizyka_Pp*0)|(Fizyka_Pr*1.5)]+[(Matematyka_Pp*0.5)|(Matematyka_Pr*1.5)]+[(Biologia_Pp*0)|(Biologia_Pr*0.5)|(Chemia_Pp*0)|(Chemia_Pr*0.5)|(Geografia_Pp*0)|(Geografia_Pr*0.5)|(Informatyka_Pp*0)|(Informatyka_Pr*0.5)]+[(Język angielski_Pp*0.2)|(Język angielski_Pr*0.4)|(Język francuski_Pp*0.2)|(Język francuski_Pr*0.4)|(Język hiszpański_Pp*0.2)|(Język hiszpański_Pr*0.4)|(Język rosyjski_Pp*0.2)|(Język rosyjski_Pr*0.4)|(Język włoski_Pp*0.2)|(Język włoski_Pr*0.4)]")
-    create(:field_detail, field_of_study: issp, recrutation_formula: "[(Fizyka_Pp*0)|(Fizyka_Pr*1.5)]+[(Matematyka_Pp*0.5)|(Matematyka_Pr*1.5)]+[(Informatyka_Pp*0)|(Informatyka_Pr*1.5)]+[(Biologia_Pp*0)|(Biologia_Pr*0.5)|(Chemia_Pp*0)|(Chemia_Pr*0.5)|(Geografia_Pp*0)|(Geografia_Pr*0.5)]+[(Język angielski_Pp*0.2)|(Język angielski_Pr*0.4)|(Język francuski_Pp*0.2)|(Język francuski_Pr*0.4)|(Język hiszpański_Pp*0.2)|(Język hiszpański_Pr*0.4)|(Język rosyjski_Pp*0.2)|(Język rosyjski_Pr*0.4)|(Język włoski_Pp*0.2)|(Język włoski_Pr*0.4)]")
-    create(:field_detail, field_of_study: biotechnology, recrutation_formula: "[(Biologia_Pp*0.35)|(Biologia_Pr*0.5)|(Chemia_Pp*0.35)|(Chemia_Pr*0.5)|(Fizyka_Pp*0.35)|(Fizyka_Pr*0.5)|(Informatyka_Pp*0.35)|(Informatyka_Pr*0.5)|(Matematyka_Pp*0.35)|(Matematyka_Pr*0.5)|(Język angielski_Pp*0.35)|(Język angielski_Pr*0.5)|(Język francuski_Pp*0.35)|(Język francuski_Pr*0.5)|(Język hiszpański_Pp*0.35)|(Język hiszpański_Pr*0.5)|(Język rosyjski_Pp*0.35)|(Język rosyjski_Pr*0.5)|(Język włoski_Pp*0.35)|(Język włoski_Pr*0.5)]+[(Biologia_Pp*0.35)|(Biologia_Pr*0.5)|(Chemia_Pp*0.35)|(Chemia_Pr*0.5)|(Fizyka_Pp*0.35)|(Fizyka_Pr*0.5)|(Informatyka_Pp*0.35)|(Informatyka_Pr*0.5)|(Matematyka_Pp*0.35)|(Matematyka_Pr*0.5)|(Język angielski_Pp*0.35)|(Język angielski_Pr*0.5)|(Język francuski_Pp*0.35)|(Język francuski_Pr*0.5)|(Język hiszpański_Pp*0.35)|(Język hiszpański_Pr*0.5)|(Język rosyjski_Pp*0.35)|(Język rosyjski_Pr*0.5)|(Język włoski_Pp*0.35)|(Język włoski_Pr*0.5)]")
-    create(:field_detail, field_of_study: med, recrutation_formula: "[(Biologia_Pp*0)|(Biologia_Pr*1)]+[(Chemia_Pp*0)|(Chemia_Pr*1)|(Fizyka_Pp*0)|(Fizyka_Pr*1)]")
-
-    create(:study_interest, user: user, field_of_study: physics)
-    create(:study_interest, user: user, field_of_study: issp)
-    create(:study_interest, user: user, field_of_study: biotechnology)
-    create(:study_interest, user: user, field_of_study: med)
+    create(:field_detail, field_of_study: @physics, recrutation_formula: "[(Fizyka_Pp*0)|(Fizyka_Pr*1.5)]+[(Matematyka_Pp*0.5)|(Matematyka_Pr*1.5)]+[(Biologia_Pp*0)|(Biologia_Pr*0.5)|(Chemia_Pp*0)|(Chemia_Pr*0.5)|(Geografia_Pp*0)|(Geografia_Pr*0.5)|(Informatyka_Pp*0)|(Informatyka_Pr*0.5)]+[(Język angielski_Pp*0.2)|(Język angielski_Pr*0.4)|(Język francuski_Pp*0.2)|(Język francuski_Pr*0.4)|(Język hiszpański_Pp*0.2)|(Język hiszpański_Pr*0.4)|(Język rosyjski_Pp*0.2)|(Język rosyjski_Pr*0.4)|(Język włoski_Pp*0.2)|(Język włoski_Pr*0.4)]")
+    create(:field_detail, field_of_study: @issp, recrutation_formula: "[(Fizyka_Pp*0)|(Fizyka_Pr*1.5)]+[(Matematyka_Pp*0.5)|(Matematyka_Pr*1.5)]+[(Informatyka_Pp*0)|(Informatyka_Pr*1.5)]+[(Biologia_Pp*0)|(Biologia_Pr*0.5)|(Chemia_Pp*0)|(Chemia_Pr*0.5)|(Geografia_Pp*0)|(Geografia_Pr*0.5)]+[(Język angielski_Pp*0.2)|(Język angielski_Pr*0.4)|(Język francuski_Pp*0.2)|(Język francuski_Pr*0.4)|(Język hiszpański_Pp*0.2)|(Język hiszpański_Pr*0.4)|(Język rosyjski_Pp*0.2)|(Język rosyjski_Pr*0.4)|(Język włoski_Pp*0.2)|(Język włoski_Pr*0.4)]")
+    create(:field_detail, field_of_study: @biotechnology, recrutation_formula: "[(Biologia_Pp*0.35)|(Biologia_Pr*0.5)|(Chemia_Pp*0.35)|(Chemia_Pr*0.5)|(Fizyka_Pp*0.35)|(Fizyka_Pr*0.5)|(Informatyka_Pp*0.35)|(Informatyka_Pr*0.5)|(Matematyka_Pp*0.35)|(Matematyka_Pr*0.5)|(Język angielski_Pp*0.35)|(Język angielski_Pr*0.5)|(Język francuski_Pp*0.35)|(Język francuski_Pr*0.5)|(Język hiszpański_Pp*0.35)|(Język hiszpański_Pr*0.5)|(Język rosyjski_Pp*0.35)|(Język rosyjski_Pr*0.5)|(Język włoski_Pp*0.35)|(Język włoski_Pr*0.5)]+[(Biologia_Pp*0.35)|(Biologia_Pr*0.5)|(Chemia_Pp*0.35)|(Chemia_Pr*0.5)|(Fizyka_Pp*0.35)|(Fizyka_Pr*0.5)|(Informatyka_Pp*0.35)|(Informatyka_Pr*0.5)|(Matematyka_Pp*0.35)|(Matematyka_Pr*0.5)|(Język angielski_Pp*0.35)|(Język angielski_Pr*0.5)|(Język francuski_Pp*0.35)|(Język francuski_Pr*0.5)|(Język hiszpański_Pp*0.35)|(Język hiszpański_Pr*0.5)|(Język rosyjski_Pp*0.35)|(Język rosyjski_Pr*0.5)|(Język włoski_Pp*0.35)|(Język włoski_Pr*0.5)]")
+    create(:field_detail, field_of_study: @med, recrutation_formula: "[(Biologia_Pp*0)|(Biologia_Pr*1)]+[(Chemia_Pp*0)|(Chemia_Pr*1)|(Fizyka_Pp*0)|(Fizyka_Pr*1)]")
   }
 
+  let(:user) { 
+    User.create!(
+      username: 'test',
+      email: 'test@example.com',
+      password: '12345678',
+      account_type: 'schoolboy',
+      matura_results: [
+        MaturaResult.create(
+          matura_subject: MaturaSubject.create(name: 'Fizyka', subject_type: :sciences),
+          level: :advanced,
+          result: 42
+        ),
+        MaturaResult.create(
+          matura_subject: MaturaSubject.create(name: 'Matematyka', subject_type: :sciences),
+          level: :basic,
+          result: 100
+        ),
+        MaturaResult.create(
+          matura_subject: MaturaSubject.create(name: 'Matematyka', subject_type: :sciences),
+          level: :advanced,
+          result: 32
+        ),
+        MaturaResult.create(
+          matura_subject: MaturaSubject.create(name: 'Informatyka', subject_type: :sciences),
+          level: :advanced,
+          result: 60
+        ),
+        MaturaResult.create(
+          matura_subject: MaturaSubject.create(name: 'Język angielski', subject_type: :humanities),
+          level: :basic,
+          result: 98
+        ),
+        MaturaResult.create(
+          matura_subject: MaturaSubject.create(name: 'Język angielski', subject_type: :humanities),
+          level: :advanced,
+          result: 78
+        )
+      ],
+      study_interests: [
+        StudyInterest.create(
+          field_of_study: @physics
+        ),
+        StudyInterest.create(
+          field_of_study: @issp
+        ),
+        StudyInterest.create(
+          field_of_study: @biotechnology
+        ),
+        StudyInterest.create(
+          field_of_study: @med
+        )
+      ]
+    )
+  }
+
+  context 'when formulas are valid' do
+    it 'returns an array' do
+      expect(RecrutationPointsCalculator.call(user)).to be_instance_of(Array)
+    end
+
+    it 'returned array contained only numbers' do
+      expect(RecrutationPointsCalculator.call(user)).to all(be_a(Numeric))
+    end
+
+    it 'correctly calculate points' do
+      expect(RecrutationPointsCalculator.call(user)).to contain_exactly(174.2, 234.2, 74, 42)
+    end
+  end
+
+  context 'when some formula is broken' do
+    before {
+      create(:field_detail, field_of_study: @med, recrutation_formula: "[(Biologia_Pp*0)|(Biologia_Pr*1)][(Chemia_Pp***0)|(Chemia_Pr*1)|(Fizyka_Pp*0)|(Fizyka_Pr*1)")
+    }
+    it 'assign N/A to this result' do
+      expect(RecrutationPointsCalculator.call(user)).to contain_exactly(174.2, 234.2, 74, 42, '-')
+    end
+  end
 end
