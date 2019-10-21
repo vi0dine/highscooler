75d35cfbabd177fb5fcb593c5e58e868aa5bd4bc
viodine <viodine@yahoo.com>
2019-10-21 19:03:16 +0200

    Another attempt to RecPointsCalc algorithm

--- app/services/recrutation_points_calculator2.rb
+++ app/services/recrutation_points_calculator2.rb
@@ -4,35 +4,35 @@ class Formula
   def initialize(formula, user)
     @formula = formula
     @exponents = build_exponents
-    @subjects = build_subjects
     @basic_results = user.basic_results
     @advanced_results = user.advanced_results
   end
 
+  attr_accessor :formula, :exponents, :basic_results, :advanced_results
+
   def calculate
     exponents.reduce(0) do |result, exponent|
-      result + exponent.max_result
+      max = exponent.max_result(basic_results, advanced_results)
+      filter_results(max)
+      result + max
     end
   end
 
   private
 
   def build_exponents
-    @formula.split('+').each do |exponent|
-      @exponents << Exponent.new(exponent)
+    formula.split('+').each do |exponent|
+      exponents << Exponent.new(exponent)
     end
   end
 
-  def calculate_exponents
-    @exponents.each do |exponent|
-      max = exponent.max_result(basic_results, advanced_results)
-      if max[:subject][:level] == :basic
-        basic_results.reject! { |res| res == max[:subject] }
-      elsif max[:subject][:level] == :advanced
-        advanced_results.reject! { |res| res == max[:subject] }
-      else
-        raise 'Wrong result level'
-      end
+  def filter_results(max)
+    if max[:subject][:level] == :basic
+      basic_results.reject! { |res| res == max[:subject] }
+    elsif max[:subject][:level] == :advanced
+      advanced_results.reject! { |res| res == max[:subject] }
+    else
+      raise 'Wrong result level'
     end
   end
 end
@@ -51,16 +51,16 @@ class Exponent
     end
   end
 
-  def max_result(basics, advanceds)
+  def max_result(basic_results, advanced_results)
     subjects
-      .map { |subject| subject.calculate(basics, advanceds) }
+      .map { |subject| subject.calculate(basic_results, advanced_results) }
       .max_by { |calculated| calculated[:result] }
   end
 end
 
 class Subject
   def initialize(subject)
-    @subject = subject.gsub!(/^[\(]|[\)]$/, '')
+    @subject = subject.gsub!(/^[(]|[)]$/, '')
     @subject_name = /(.*)_/.match(@subject)[1].to_s
     @level = /.*_(..)/.match(@subject)[1].to_s == 'Pp' ? :basic : :advanced
     @factor = /.*\*(.*)/.match(@subject)[1].to_f
@@ -68,28 +68,28 @@ class Subject
 
   attr_reader :subject_name, :level, :factor
 
-  def calculate(basics, advanceds)
-    { subject: find_user_result(basics, advanceds),
-      result: user_result(basics, advanceds) * factor }
+  def calculate(basic_results, advanced_results)
+    { subject: find_user_result(basic_results, advanced_results),
+      result: user_result(basic_results, advanced_results) * factor }
   end
 
   private
 
-  def find_user_result(basics, advanceds)
+  def find_user_result(basic_results, advanced_results)
     if level == :basic
-      basics.find { |result| result.matura_subject.name == subject_name }
+      basic_results.find { |result| result.matura_subject.name == subject_name }
     elsif level == :advanced
-      advanceds.find { |result| result.matura_subject.name == subject_name }
+      advanced_results.find { |result| result.matura_subject.name == subject_name }
     else
       raise StandardError, 'Wrong subject level'
     end
   end
 
-  def user_result(basics, advanceds)
-    if find_user_result(basics, advanceds).nil?
+  def user_result(basic_results, advanced_results)
+    if find_user_result(basic_results, advanced_results).nil?
       0
     else
-      find_user_result(basics, advanceds).result
+      find_user_result(basic_results, advanced_results).result
     end
   end
 end
